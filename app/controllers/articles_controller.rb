12794a4bd735443dbcef5893fb8bbb0b66310d0b
Hiroyuk Ishizuka <hiroyuki.1207.18@gmail.com>
2021-02-09 16:15:49 +0900

    [create]controller_action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,27 +1,50 @@
 class ArticlesController < ApplicationController
+  before_action :find_post, only: (:show :edit :update :destroy)
   def index
+  	@posts = Post.order(created_at: :desc)
   end
 
   def show
+  	@post = Post.find(params[:id])
   end
 
   def edit
   end
 
   def new
+  	@article = Article.new
   end
 
   def create
-  	
+  	@article = Article.new(article_params)
+  	if @article.save
+  		redirect_to @article, notice: '作成に成功しました。'
+  	else
+  		render :new, alert: '作成できませんでした。'
+  	end
   end
 
   def update
-  	
+  	if @article.update(artilce_params)
+  		redirect_to @artilce, notice: '更新に成功しました。'
+  	else
+  		render :edit, alert: '更新できませんでした。'
   end
 
   def destroy
-  	
+  	if @article.destroy
+  		redirect_to root_path, notice: '削除に成功しました。'
+  	else
+  		redirect_to root_path, alert: '削除できませんでした。'
+  	end
   end
 
-  
+  private
+  def find_post
+  	@post = Post.find(params[:id])
+  end
+
+  def artilce_params
+  	
+  end
 end
