9f8b71605c348b31d21c9b5d509f1c542139b6c7
Hiroyuk Ishizuka <hiroyuki.1207.18@gmail.com>
2021-02-15 10:44:24 +0900

    [install]devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :installs
+  devise_for :users
   resources :articles
-  root to: "articles#index"
+  root "articles#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
