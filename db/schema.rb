e6ea4f9b59923d70173b02cee3ee14a4c0eac3d2
Hiroyuk Ishizuka <hiroyuki.1207.18@gmail.com>
2021-02-21 16:45:46 +0900

    git commit --amend -m "[edit]add_user_id_to_articles_rb"

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_16_031507) do
+ActiveRecord::Schema.define(version: 2021_02_21_074441) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
   create_table "profiles", force: :cascade do |t|
