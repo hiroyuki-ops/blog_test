7f3d4ef3c3609fab9c5d8f07468fbf61323314ad
Hiroyuk Ishizuka <hiroyuki.1207.18@gmail.com>
2021-02-21 17:10:46 +0900

    [add]user.registration.function

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one :profile, dependent: :destroy
+  has_many :articles, dependent: :destroy
 
   delegate :name, to: :profile, prefix: :auther, allow_nil: true
 end
